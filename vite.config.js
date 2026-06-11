042568c9251a01fc56114a5e1a7dcb41cc1dac94
timbone72-CC <timbone72@gmail.com>
2026-06-11 09:09:23 -0500

    Update Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/crewpay-field-app/",
+  base: "/crewpay-worker-field-app/",
   build: {
     outDir: "dist",
   },
