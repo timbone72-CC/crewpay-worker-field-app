a98608d40d270c89c16f1c32e7f829b5bd56c5c2
timbone72-CC <timbone72@gmail.com>
2026-06-14 12:45:09 -0600

    Add CrewPay bridge pending queue storage key

--- src/shared/constants/storageKeys.js
+++ src/shared/constants/storageKeys.js
@@ -1,5 +1,6 @@
 export const STORAGE_KEYS = {
   ACTIVE_PAY_PERIOD: "fieldledger.activePayPeriod",
+  BRIDGE_PENDING_TIME_ENTRIES: "fieldledger.bridgePendingTimeEntries",
   JOB_SUGGESTIONS: "fieldledger.jobSuggestions",
   SETTINGS: "fieldledger.settings",
   TRUSTED_SHEET_WEB_APP_URL: "fieldledger.trustedSheetWebAppUrl",
